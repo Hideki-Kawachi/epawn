d8c0a8c2c77020ccecc5ebd3f052ce1d1c2555df
Hideki-Kawachi <hideki.kawachi17@gmail.com>
2023-02-21 15:23:15 +0800

    Modularized cancel modal

--- components/modals/cancel.js
+++ components/modals/cancel.js
@@ -1,40 +1,39 @@
 import React from "react";
 import Close from "../closebutton";
-import {useRouter} from "next/router";
-export const Cancel = ({ trigger, setTrigger }) => {
-const router = useRouter()
-  function closeModal() {
-    setTrigger(!trigger);
-  }
+import { useRouter } from "next/router";
 
-  return (
-    <>
-      <div id="modal-content-area">
-        <div className="bg-gray-100 border-2 rounded-xl px-20 pb-10 pt-10 min-w-fit">
-          <div className="ml-[615px] mb-5" onClick={closeModal}>
-            <Close />
-          </div>
-          <p className="font-nunito text-base text-center">
-            Are you sure you want to cancel <b> Redemption</b> of <br />
-            <b>Pawn Ticket A-123456</b>? <br /> <br />
-            All unsubmitted data will be lost.
-          </p>
-          <button
-            className="bg-green-300 text-base px-24 mt-10 mx-5"
-            onClick={closeModal}
-          >
-            Back
-          </button>
-          <button
-            className="bg-red-300 text-base px-24 mt-10 mx-5"
-            onClick={() => router.push("/")}
-          >
-            Cancel
-          </button>
-        </div>
-      </div>
-    </>
-  );
+export const Cancel = ({ trigger, setTrigger, content }) => {
+	const router = useRouter();
+	function closeModal() {
+		setTrigger(!trigger);
+	}
+
+	return (
+		<>
+			<div id="modal-content-area">
+				<div className="flex flex-col items-center justify-center px-20 pt-10 pb-10 bg-gray-100 border-2 rounded-xl min-w-fit">
+					<div className="ml-[615px] mb-5" onClick={closeModal}>
+						<Close />
+					</div>
+					<p className="text-base text-center font-nunito">{content}</p>
+					<div className="flex gap-10 mt-10">
+						<button
+							className="px-24 text-base bg-green-300"
+							onClick={closeModal}
+						>
+							Back
+						</button>
+						<button
+							className="px-24 text-base bg-red-300"
+							onClick={() => router.push("/")}
+						>
+							Cancel
+						</button>
+					</div>
+				</div>
+			</div>
+		</>
+	);
 };
 
 export default Cancel;
--- pages/redeem/index.js
+++ pages/redeem/index.js
@@ -1,4 +1,4 @@
-import React, {useState} from "react";
+import React, { useState } from "react";
 import Header from "../../components/header";
 import NavBar from "../../components/navigation/navBar";
 import DetailsCard from "../../components/redeem/detailscard";
@@ -10,108 +10,122 @@ import Cancel from "../../components/modals/cancel";
 import PawnDetails from "../../components/modals/pawnDetails";
 
 function Redeem() {
-  // Modals
-  const [submitModal, setSubmitOpen] = useState(false); //Submit
-  const [cancelModal, setCancelOpen] = useState(false); //Cancel
-  const [customerModal, setCustomerOpen] = useState(false); //View Customer Details
-  const [historyModal, setHistoryOpen] = useState(false); //Pawn History
- 
+	// Modals
+	const [submitModal, setSubmitOpen] = useState(false); //Submit
+	const [cancelModal, setCancelOpen] = useState(false); //Cancel
+	const [customerModal, setCustomerOpen] = useState(false); //View Customer Details
+	const [historyModal, setHistoryOpen] = useState(false); //Pawn History
+	const [PTNumber, setPTNumber] = useState("A-123456");
 
+	function submitForm() {
+		setSubmitOpen(true);
+	}
 
-  function submitForm() {
-    setSubmitOpen(true);
-  }
+	function cancelForm() {
+		setCancelOpen(true);
+	}
 
-    function cancelForm() {
-      setCancelOpen(true);
-    }
-  return (
-    <>
-      <NavBar></NavBar>
-      <Header currentUser={"Kawachi, Hideki"}></Header>
-      {/* First Half */}
+	function cancelContentShow() {
+		return (
+			<>
+				Are you sure you want to cancel <b> Redemption</b> of <br />
+				<b>{PTNumber}</b>? <br /> <br />
+				All unsubmitted data will be lost.
+			</>
+		);
+	}
 
-      <Modal isOpen={submitModal} ariaHideApp={false} className="modal">
-        <Submit trigger={submitModal} setTrigger={setSubmitOpen} />
-      </Modal>
+	return (
+		<>
+			<NavBar></NavBar>
+			<Header currentUser={"Kawachi, Hideki"}></Header>
+			{/* First Half */}
 
-      <Modal isOpen={cancelModal} ariaHideApp={false} className="modal">
-        <Cancel trigger={cancelModal} setTrigger={setCancelOpen} />
-      </Modal>
+			<Modal isOpen={submitModal} ariaHideApp={false} className="modal">
+				<Submit trigger={submitModal} setTrigger={setSubmitOpen} />
+			</Modal>
 
-      <div id="main-content-area" className="flex-col bg-gray-150">
-        <p className="font-dosis text-green-500 text-xl font-semibold underline mb-5">
-          Redeem
-        </p>
-        <div className="flex">
-          <DetailsCard></DetailsCard>
-        </div>
+			<Modal isOpen={cancelModal} ariaHideApp={false} className="modal">
+				<Cancel
+					trigger={cancelModal}
+					setTrigger={setCancelOpen}
+					content={cancelContentShow()}
+				/>
+			</Modal>
 
-        {/* Second Half */}
+			<div id="main-content-area" className="flex-col bg-gray-150">
+				<p className="mb-5 text-xl font-semibold text-green-500 underline font-dosis">
+					Redeem
+				</p>
+				<div className="flex">
+					<DetailsCard></DetailsCard>
+				</div>
 
-        <div className="flex">
-          {/* Remaining Items  */}
+				{/* Second Half */}
 
-          <div className="mt-20">
-            <p className="font-nunito font-bold text-base ml-10">
-              Remaining Items:{" "}
-            </p>
-            {/* plan: CheckItem is ItemCard w/ Check*/}
-            <div className="bg-white p-5 mx-10 h-96 overflow-y-scroll border-2">
-              {/* plan: CheckItem & ItemCard section will be generated using .map */}
-              <CheckItem></CheckItem>
-              <CheckItem></CheckItem>
-              <CheckItem></CheckItem>
-              <CheckItem></CheckItem>
-            </div>
-            <div className="bg-gray-200 mx-10 rounded-b-xl">
-              <div className="py-3">
-                <section className="ml-80">
-                  <span className="ml-20 mr-10 font-bold font-nunito">
-                    Selected (0){" "}
-                  </span>
-                  <button className="bg-green-300 text-white">
-                    Add to Redeem
-                  </button>
-                </section>
-              </div>
-            </div>
-          </div>
+				<div className="flex">
+					{/* Remaining Items  */}
 
-          {/*Items for Redemption */}
-          <div className="mt-20 ">
-            <p className="font-nunito font-bold  text-base ml-10">
-              Items for Redemption:{" "}
-            </p>
-            <div className="bg-white p-5 mx-10 max-h-[450px] overflow-y-scroll border-2">
-              <DeleteItem></DeleteItem>
-              <DeleteItem></DeleteItem>
-              <DeleteItem></DeleteItem>
-              <DeleteItem></DeleteItem>
-            </div>
-          </div>
-        </div>
-        <div className="mt-5 flex flex-row ml-[1180px]">
-          <div>
-            <button
-              className="bg-red-300 text-white text-base px-10 mx-2 my-5"
-              onClick={cancelForm}
-            >
-              Cancel
-            </button>
-          </div>
-          <div>
-            <button
-              className="bg-green-300 text-white text-base px-10 mx-2 my-5"
-              onClick={submitForm}
-            >
-              Submit
-            </button>
-          </div>
-        </div>
-      </div>
-    </>
-  );
+					<div className="mt-20">
+						<p className="ml-10 text-base font-bold font-nunito">
+							Remaining Items:{" "}
+						</p>
+						{/* plan: CheckItem is ItemCard w/ Check*/}
+						<div className="p-5 mx-10 overflow-y-scroll bg-white border-2 h-96">
+							{/* plan: CheckItem & ItemCard section will be generated using .map */}
+							<CheckItem></CheckItem>
+							<CheckItem></CheckItem>
+							<CheckItem></CheckItem>
+							<CheckItem></CheckItem>
+						</div>
+						<div className="mx-10 bg-gray-200 rounded-b-xl">
+							<div className="py-3">
+								<section className="ml-80">
+									<span className="ml-20 mr-10 font-bold font-nunito">
+										Selected (0){" "}
+									</span>
+									<button className="text-white bg-green-300">
+										Add to Redeem
+									</button>
+								</section>
+							</div>
+						</div>
+					</div>
+
+					{/*Items for Redemption */}
+					<div className="mt-20 ">
+						<p className="ml-10 text-base font-bold font-nunito">
+							Items for Redemption:{" "}
+						</p>
+						<div className="bg-white p-5 mx-10 max-h-[450px] overflow-y-scroll border-2">
+							<DeleteItem></DeleteItem>
+							<DeleteItem></DeleteItem>
+							<DeleteItem></DeleteItem>
+							<DeleteItem></DeleteItem>
+						</div>
+					</div>
+				</div>
+				<div className="mt-5 flex flex-row ml-[1180px]">
+					<div>
+						<button
+							className="px-10 mx-2 my-5 text-base text-white bg-red-300"
+							onClick={cancelForm}
+						>
+							Cancel
+						</button>
+					</div>
+					<div>
+						<button
+							className="px-10 mx-2 my-5 text-base text-white bg-green-300"
+							onClick={submitForm}
+						>
+							Submit
+						</button>
+					</div>
+				</div>
+			</div>
+		</>
+	);
 }
 
 export default Redeem;
