f44672475fb0c70830406fc92cecad1798737526
Hideki-Kawachi <hideki.kawachi17@gmail.com>
2023-02-24 00:19:22 +0800

    Working Notif without Filter

---
+++ pages/api/notifTable.js
@@ -0,0 +1,23 @@
+import mongoose from "mongoose";
+import Test from "../../schemas/test";
+import dbConnect from "../../utilities/dbConnect";
+
+export default async function NotifTable(req, res) {
+	dbConnect();
+
+	let all = null;
+
+	const changeStream = await Test.watch().on("change", async (data) => {
+		console.log("hahu");
+		all = await Test.find({});
+		console.log("all", all);
+
+		if (all) {
+			console.log("ALL IS:", all);
+			await changeStream.close();
+			res.json(all);
+		} else {
+			console.log("NER", all);
+		}
+	});
+}
--- pages/index.js
+++ pages/index.js
@@ -1,18 +1,73 @@
 import Head from "next/head";
 import Link from "next/link";
-import React, { useState } from "react";
+import React, { useEffect, useState } from "react";
 import ClerkHome from "../components/home/clerkHome";
 import Header from "../components/header";
 import ManagerHome from "../components/home/managerHome";
 import NavBar from "../components/navigation/navBar";
+import dbConnect from "../utilities/dbConnect";
+import mongoose from "mongoose";
+import Test from "../schemas/test";
+import NotifTable from "./api/notifTable";
 
-export default function Home() {
+export async function getServerSideProps() {
+	// dbConnect();
+	let all = {};
+	// console.log("server side props");
+	// Test.watch().on("change", async (data) => {
+	// 	let all = await Test.find({});
+	// 	console.log("on change NotifTable", all);
+	// 	return { props: { message: all } };
+	// });
+
+	return { props: { message: all } };
+}
+
+export default function Home({ message }) {
 	const [role, setRole] = useState("clerk");
+	const [showData, setShowData] = useState({});
 
 	const roleShow = {
 		manager: <ManagerHome></ManagerHome>,
 		clerk: <ClerkHome></ClerkHome>,
 	};
+
+	function buttonClick() {
+		fetch("/api/buttonClick", {
+			method: "POST",
+		})
+			.then((res) => res.json())
+			.then((data) => {
+				console.log("POST DATA IS:", data);
+			});
+	}
+
+	useEffect(() => {
+		waitNotif();
+	}, []);
+
+	async function waitNotif() {
+		console.log("wait notif");
+		let res = await fetch("/api/notifTable", {
+			method: "GET",
+		});
+
+		if (res.status == 502) {
+			await waitNotif();
+		} else if (res.status != 200) {
+			console.log("2-RESPONSE:", res.statusText);
+			await new Promise((resolve) => setTimeout(resolve, 1000));
+		} else {
+			let message = await res.text();
+			setShowData(message);
+
+			await waitNotif();
+		}
+	}
+
+	useEffect(() => {
+		console.log("HERE IS THE RESULT:", showData);
+	}, [showData]);
 
 	return (
 		<div>
@@ -27,6 +82,7 @@ export default function Home() {
 			<Header currentUser={"Kawachi, Hideki"}></Header>
 			<NavBar></NavBar>
 			{roleShow[role]}
+			<button onClick={() => buttonClick()}>HELLO WORLD</button>
 		</div>
 	);
 }
