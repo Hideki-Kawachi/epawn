98cd69342798d4e8370633951f0f9fb581053e30
Hideki-Kawachi <hideki.kawachi17@gmail.com>
2023-02-23 12:49:27 +0800

    working sorting and filtering-ish
    transaction and status filter still not fully implemented

--- components/home/notificationTable.js
+++ components/home/notificationTable.js
@@ -13,12 +13,13 @@ function NotificationTable({ data }) {
 			{
 				Header: "Transaction",
 				accessor: "transaction",
+				disableGlobalFilter: true,
 			},
 			{ Header: "Customer Name", accessor: "customerName" },
 			{ Header: "PT Number", accessor: "ptNumber" },
-			{ Header: "Date", accessor: "date" },
-			{ Header: "Time", accessor: "time" },
-			{ Header: "Status", accessor: "status" },
+			{ Header: "Date", accessor: "date", disableGlobalFilter: true },
+			{ Header: "Time", accessor: "time", disableGlobalFilter: true },
+			{ Header: "Status", accessor: "status", disableGlobalFilter: true },
 		],
 		[]
 	);
@@ -37,6 +38,7 @@ function NotificationTable({ data }) {
 		nextPage,
 		previousPage,
 		setGlobalFilter,
+		setFilter,
 		state: { pageIndex },
 	} = useTable(
 		{
@@ -52,18 +54,76 @@ function NotificationTable({ data }) {
 
 	return (
 		<>
+			<div className="flex items-center justify-center w-3/4 gap-2 my-5 text-base font-nunito">
+				<span className="text-base">Search: </span>
+				<input
+					className="flex-grow"
+					onChange={(e) => {
+						setGlobalFilter(e.target.value);
+					}}
+					placeholder={"PT Number or Customer Name"}
+				/>
+				<span className="ml-5">Transaction: </span>
+				<select
+					className="h-fit"
+					onChange={(e) => setFilter("transaction", e.target.value)}
+				>
+					<option value={""} selected>
+						All
+					</option>
+					<option value={"Pawn"}>Pawn</option>
+					<option value={"Renew"}>Renew</option>
+					<option value={"Redeem"}>Redeem</option>
+				</select>
+				<span className="ml-5">Status: </span>
+				<select
+					className="h-fit"
+					onChange={(e) => setFilter("status", e.target.value)}
+				>
+					<option value={""} selected>
+						All
+					</option>
+					<option value={"Pawn"}>Pawn</option>
+					<option value={"Renew"}>Renew</option>
+					<option value={"Redeem"}>Redeem</option>
+				</select>
+			</div>
 			<table {...getTableProps()} className="w-3/4 text-base">
 				<thead>
 					{headerGroups.map((headerGroup) => (
 						<tr {...headerGroup.getHeaderGroupProps()}>
-							{headerGroup.headers.map((column) => (
-								<th
-									{...column.getHeaderProps(column.getSortByToggleProps())}
-									className="border-4 border-gray-500 border-solid"
-								>
-									{column.render("Header")}
-								</th>
-							))}
+							{headerGroup.headers.map((column) => {
+								if (
+									column.Header !== "Transaction" &&
+									column.Header.toString() !== "Status"
+								) {
+									console.log("NOT HEADER", column.Header);
+									return (
+										<th
+											{...column.getHeaderProps(column.getSortByToggleProps())}
+											className="border-4 border-gray-500 border-solid"
+										>
+											{column.render("Header")}
+											<span className="ml-2 text-base">
+												{column.isSorted
+													? column.isSortedDesc
+														? "↑"
+														: "↓"
+													: ""}
+											</span>
+										</th>
+									);
+								}
+
+								return (
+									<th
+										{...column.getHeaderProps()}
+										className="border-4 border-gray-500 border-solid"
+									>
+										{column.render("Header")}
+									</th>
+								);
+							})}
 						</tr>
 					))}
 				</thead>
