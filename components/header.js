592b4be1def4e9d4a2f32ced5d71fdb1e5cb5a32
Hideki-Kawachi <hideki.kawachi17@gmail.com>
2023-03-02 19:24:14 +0800

    updated header to include role

--- components/header.js
+++ components/header.js
@@ -15,10 +15,15 @@ function Header({ currentUser }) {
 	}
 
 	return (
-		<header className="absolute right-5 top-5 w-fit h-fit">
-			<span className="text-base font-semibold font-nunito">
-				{currentUser.lastName}, {currentUser.firstName} |{" "}
-			</span>
+		<header className="absolute flex items-center justify-center gap-3 right-5 top-5 w-fit h-fit">
+			<div className="flex flex-col items-end justify-end pr-3 space-y-[-5px] border-r-2 border-gray-500 w-fit h-fit">
+				<span className="text-base font-semibold font-nunito">
+					{currentUser.lastName}, {currentUser.firstName}
+				</span>
+				<span className="text-sm capitalize font-nunito">
+					{currentUser.role}
+				</span>
+			</div>
 			<a
 				className="text-base font-semibold text-red-500 cursor-pointer font-nunito hover:underline"
 				onClick={() => logout()}
