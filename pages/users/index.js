ab85c376b00b9f3beb94b3457741fb8090aae1fb
carlocortez <carlo_miguel_cortez@dlsu.edu.ph>
2023-03-12 22:48:47 +0800

    Added fetching for current list in DB

--- pages/users/index.js
+++ pages/users/index.js
@@ -6,8 +6,10 @@ import UserCreate from "../../components/users/UserCreate";
 import { useState } from "react";
 import { withIronSessionSsr } from "iron-session/next";
 import { ironOptions } from "../../utilities/config";
+import dbConnect from "../../utilities/dbConnect";
 
 import MockUsers from "../../components/users/User_MOCK_DATA.json";
+import User from "../../schemas/user";
 
 export const getServerSideProps = withIronSessionSsr(
 	async function getServerSideProps({ req }) {
@@ -21,11 +23,33 @@ export const getServerSideProps = withIronSessionSsr(
 			req.session.userData.role == "manager"
 		) {
 
+			await dbConnect();
+
+			const userList = await User.find(
+				{},
+				{ userID: 1, firstName: 1, lastName: 1, role: 1, isDisabled: 1}
+			)
+
+			var tempUserData = [];
+
+			userList.forEach( (user) => {
+				tempUserData.push({
+					userID: user.userID,
+					firstName: user.firstName,
+					lastName: user.lastName,
+					roleID: user.role,
+					isDisabled: user.isDisabled,
+				})
+			});
 			// const res = await fetch("../components/users/User_MOCK_DATA.json")
   			// const MockUsers = await res.json()
 
+			let userData = JSON.stringify(tempUserData);
+
+			console.log(userData);
+
 			return {
-				props: { currentUser: req.session.userData},
+				props: { currentUser: req.session.userData, userData},
 			};
 		} else if (req.session.userData.role == "customer") {
 			return {
@@ -42,11 +66,11 @@ export const getServerSideProps = withIronSessionSsr(
 	ironOptions
 );
 
-function Users({ currentUser}) {
+function Users({ currentUser, userData}) {
 
 	// const [userList, setUserList] = useState(MockUsers);
 	
-	// const users = JSON.parse(userData);
+	const users = JSON.parse(userData);
 	// const roles = JSON.parse(roleData);
 
 	// const users = JSON.parse()
@@ -58,7 +82,7 @@ function Users({ currentUser}) {
 	// const [rightShow, setRightShow] = useState("button");
 	// const [isEditing, setIsEditing] = useState("");
 	// const [isViewing, setIsViewing] = useState("");
-	// const [userShow, setUserShow] = useState(users);
+	const [userShow, setUserShow] = useState(users);
 	// const [notifResult, setNotifResult] = useState("");
 
 	return (
@@ -97,13 +121,13 @@ function Users({ currentUser}) {
 				{/* <UserCard firstName={"Sulletta"} lastName={"Mercury"} roleName={"Manager"}></UserCard> */}
 
 				<div className="grid grid-cols-3 m-10 overflow-y-scroll bg-white h-60">
-					{MockUsers.map((mockUser) => {
+					{userShow.map((mockUser) => {
 						return (
 							<UserCard
 								key={mockUser.userID}
 								firstName={mockUser.firstName}
 								lastName={mockUser.lastName}
-								roleName={mockUser.roleName}
+								roleName={mockUser.roleID}
 								userID={mockUser.userID}
 								isDisabled={mockUser.isDisabled}
 							></UserCard>
