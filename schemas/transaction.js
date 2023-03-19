0bcb2d3736833372ed90e98159d3a96bb78de210
MartinJ-13 <80908021+MartinJ-13@users.noreply.github.com>
2023-03-19 21:17:01 +0800

    added default in transaction schema

--- schemas/transaction.js
+++ schemas/transaction.js
@@ -1,19 +1,19 @@
 import mongoose from "mongoose";
 
 const TransactionSchema = new mongoose.Schema(
-	{
-		customerID: { type: String, require: true },
-		branchID: { type: Number, require: true },
-		itemListID: { type: Number },
-		transactionType: { type: String, require: true },
-		clerkID: { type: String, require: true },
-		managerID: { type: String, require: true },
-		creationDate: { type: Date, require: true, default: new Date() },
-		status: { type: String },
-		rejectionMessage: { type: String },
-		amountPaid: { type: Number },
-	},
-	{ timestamps: true }
+  {
+    customerID: { type: String, require: true },
+    branchID: { type: Number, require: true },
+    itemListID: { type: Number, default: "" },
+    transactionType: { type: String, require: true, default: "" },
+    clerkID: { type: String, require: true, default: "" },
+    managerID: { type: String, require: true, default: "" },
+    creationDate: { type: Date, require: true, default: new Date() },
+    status: { type: String, default: "" },
+    rejectionMessage: { type: String, default: "" },
+    amountPaid: { type: Number, default: "" },
+  },
+  { timestamps: true }
 );
 
 let Transaction;
