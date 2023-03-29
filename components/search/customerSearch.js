aeb0a7e256c9f24e9037e622c28d7a1adce43058
Hideki-Kawachi <hideki.kawachi17@gmail.com>
2023-03-29 13:37:50 +0800

    Added skeleton for search

---
+++ components/search/customerSearch.js
@@ -0,0 +1,11 @@
+import React from "react";
+
+function CustomerSearch() {
+	return (
+		<div>
+			<span>CUSTOMER Search</span>
+		</div>
+	);
+}
+
+export default CustomerSearch;
--- pages/search/index.js
+++ pages/search/index.js
@@ -1,4 +1,4 @@
-import React from "react";
+import React, { useEffect, useState } from "react";
 import Header from "../../components/header";
 import NavBar from "../../components/navigation/navBar";
 import { withIronSessionSsr } from "iron-session/next";
@@ -31,12 +31,69 @@ export const getServerSideProps = withIronSessionSsr(
 );
 
 function Search({ currentUser }) {
+	const [showTab, setShowTab] = useState("PawnTicket");
+
+	useEffect(() => {
+		if (showTab == "PawnTicket") {
+			document.getElementById("PawnTicket-Tab").style.backgroundColor = "white";
+			document.getElementById("Customer-Tab").style.backgroundColor =
+				"rgb(205 204 202 / var(--tw-bg-opacity))";
+			document.getElementById("Items-Tab").style.backgroundColor =
+				"rgb(205 204 202 / var(--tw-bg-opacity))";
+		} else if (showTab == "Customer") {
+			document.getElementById("PawnTicket-Tab").style.backgroundColor =
+				"rgb(205 204 202 / var(--tw-bg-opacity))";
+			document.getElementById("Customer-Tab").style.backgroundColor = "white";
+			document.getElementById("Items-Tab").style.backgroundColor =
+				"rgb(205 204 202 / var(--tw-bg-opacity))";
+		} else if (showTab == "Items") {
+			document.getElementById("PawnTicket-Tab").style.backgroundColor =
+				"rgb(205 204 202 / var(--tw-bg-opacity))";
+			document.getElementById("Customer-Tab").style.backgroundColor =
+				"rgb(205 204 202 / var(--tw-bg-opacity))";
+			document.getElementById("Items-Tab").style.backgroundColor = "white";
+		}
+	}, [showTab]);
+
+	const displayTab = {
+		PawnTicket: <span>Pawn Ticket</span>,
+		Customer: <span>Customer</span>,
+		Items: <span>Items</span>,
+	};
+
 	return (
 		<>
 			<NavBar currentUser={currentUser}></NavBar>
 			<Header currentUser={currentUser}></Header>
 			<div id="main-content-area">
-				<p>Search</p>
+				<div className="w-full h-full">
+					<div className="flex flex-row gap-4 text-base font-semibold text-green-500">
+						<div
+							id="PawnTicket-Tab"
+							className="p-3 bg-gray-200 border-2 rounded cursor-pointer w-fit"
+							onClick={() => setShowTab("PawnTicket")}
+						>
+							PawnTicket
+						</div>
+						<div
+							id="Customer-Tab"
+							className="p-3 bg-gray-200 border-2 rounded cursor-pointer w-fit"
+							onClick={() => setShowTab("Customer")}
+						>
+							Customer
+						</div>
+						<div
+							id="Items-Tab"
+							className="p-3 bg-gray-200 border-2 rounded cursor-pointer w-fit"
+							onClick={() => setShowTab("Items")}
+						>
+							Items
+						</div>
+					</div>
+				</div>
+				<div className="w-full h-full bg-white border-2 mt-[-5px] p-5">
+					{displayTab[showTab]}
+				</div>
 			</div>
 		</>
 	);
