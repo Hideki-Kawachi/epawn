26b41aceea4570067efda4b0d60943262b19ded8
carlocortez <carlo_miguel_cortez@dlsu.edu.ph>
2023-04-07 21:45:03 +0800

    Updated netcashflow results

--- components/reports/cashFlowReport.js
+++ components/reports/cashFlowReport.js
@@ -79,13 +79,24 @@ function CashFlowReport({
 					tempCashOut = currTransaction.amountPaid.toFixed(2)
 				}
 
-				tempData.push({
-					branchName: currBranch.branchName, 
-					transactDate: dayjs(new Date(currTransaction.creationDate)).format("MMM DD, YYYY"),
-					cashInAmount: tempCashIn,
-					cashOutAmount: tempCashOut,
-					netCashFlow: (parseFloat(tempCashIn) + parseFloat(tempCashOut)).toFixed(2),
-				})
+				if (tempData.length == 0) {
+					tempData.push({
+						branchName: currBranch.branchName, 
+						transactDate: dayjs(new Date(currTransaction.creationDate)).format("MMM DD, YYYY"),
+						cashInAmount: tempCashIn,
+						cashOutAmount: tempCashOut,
+						netCashFlow: (parseFloat(tempCashIn) + parseFloat(tempCashOut)).toFixed(2),
+					})
+				} else {
+					tempData.push({
+						branchName: currBranch.branchName, 
+						transactDate: dayjs(new Date(currTransaction.creationDate)).format("MMM DD, YYYY"),
+						cashInAmount: tempCashIn,
+						cashOutAmount: tempCashOut,
+						netCashFlow: (parseFloat(tempData[tempData.length - 1].netCashFlow) + parseFloat(tempCashIn) + parseFloat(tempCashOut)).toFixed(2),
+					})
+						
+				}
 					
 			}
 			else {
@@ -105,13 +116,13 @@ function CashFlowReport({
 					tempData[index].cashOutAmount = newVal.toFixed(2)
 				}
 
-				if (index != 0){
-					newTotal = parseFloat(tempData[index - 1].netCashFlow) + parseFloat(currTransaction.amountPaid)
-					tempData[index].netCashFlow = newTotal.toFixed(2)
-				} else {
+				// if (index != 0){
+				// 	newTotal = parseFloat(tempData[index - 1].netCashFlow) + parseFloat(currTransaction.amountPaid)
+				// 	tempData[index].netCashFlow = newTotal.toFixed(2)
+				// } else {
 					newTotal = parseFloat(tempData[index].netCashFlow) + parseFloat(currTransaction.amountPaid)
 					tempData[index].netCashFlow = newTotal.toFixed(2)
-				}
+				// }
 
 
 			}
